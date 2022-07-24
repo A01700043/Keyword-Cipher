a4665cd981da94eba6f3f5aedc29c01ccfada123
A01700043 <33466790+A01700043@users.noreply.github.com>
2022-07-24 13:50:54 -0500

    Add files via upload

--- Code/cipher.ex
+++ Code/cipher.ex
@@ -1,66 +1,104 @@
 defmodule  Cipher do
-  # c"cipher.ex"
-  # Cipher.main("input1.txt","Dragon")
-  def main(input,keyword)
-  do
-    input
-    |>Cipher.read_contents()
-    |>Cipher.encryption_process(keyword)
+
+  # Cipher.main("input1.txt","output1.txt","Dragon", :encrypt)
+ def main(inputf,outputf,keyword,operation)
+ do
+    case operation
+      do
+        :encrypt->
+          inputf
+          |>Cipher.read_contents()
+          |>Cipher.encryption_process(keyword)
+          |>Cipher.store_txt(outputf)
+       :decrypt-> "Element Alphabet 2"
+        _ -> "Unrecognized Operation"
+    end
+ end
+  #Complete!
+  def read_contents(inputf)
+    do
+     File.stream!(inputf)
+     |>Enum.map(fn x -> String.trim(x)end)
+     |>Enum.map(fn x -> String.split(x, ",")end)
   end
-
-  def read_contents(list)
+  # Complete!
+  def encryption_process(data,keyword)
   do
-  File.stream!(list)
-  |>Enum.map(fn x -> String.trim(x)end)
-  |>Enum.map(fn x -> String.split(x, ",")end)
+    Enum.map(data,fn [x] -> String.split(x ,"", trim: true)end)
+     |>Enum.map(fn x -> Cipher.letter_parser(x,Cipher.alphabet_encryption(keyword))end)
+     |>Enum.map(fn x -> List.to_string(x)end)
+     |>Cipher.data_parser()
   end
-
-  def encryption_process(content,keyword)
-  do
-
-  alfabeto=Cipher.alphabet_encryption(keyword)
-  newcontent=Enum.map(content,fn [x] -> String.split(x ,"", trim: true)end)
-  dos=Enum.map(newcontent,fn x -> Cipher.letter_encryption(x,alfabeto)end)
-  trp=Enum.map(dos,fn x -> List.to_string(x)end)
-  cosas=Cipher.data_parser(trp)
-  Cipher.store_txt(cosas)
-  end
-
+  # Complete!
   def data_parser(data)
   do
     data
     |>Enum.map_join( "\n",&(&1))
   end
-
-  def alphabet_encryption(keyword)
+  # Complete!
+def alphabet_encryption(keyword)
+   do
+   Enum.uniq(
+     (
+     keyword
+     |>String.upcase()
+     |>String.split("", trim: true))
+     ++ Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>
+    end))
+end
+   # Complete!
+def letter_parser(letter,alphabet)
 do
- kw = keyword |>String.upcase() |>String.split("", trim: true) |>Enum.uniq()
- Enum.uniq( kw ++ Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>> end))
-
+  Enum.map(letter,fn x -> x
+    if String.match?(x, ~r/^\p{L}$/u)
+    do
+      if (x === String.upcase(x))
+      do
+        Cipher.letter_encryption(x, alphabet)
+      else
+        String.downcase(Cipher.letter_encryption(String.upcase(x), alphabet))
+       end
+    else
+      x
+    end
+  end)
+end
+   #Complete!
+def letter_encryption(letter, alphabet) do
+  case letter do
+    "A" -> Enum.at(alphabet, 0)
+    "B" -> Enum.at(alphabet, 1)
+    "C" -> Enum.at(alphabet, 2)
+    "D" -> Enum.at(alphabet, 3)
+    "E" -> Enum.at(alphabet, 4)
+    "F" -> Enum.at(alphabet, 5)
+    "G" -> Enum.at(alphabet, 6)
+    "H" -> Enum.at(alphabet, 7)
+    "I" -> Enum.at(alphabet, 8)
+    "J" -> Enum.at(alphabet, 9)
+    "K" -> Enum.at(alphabet, 10)
+    "L" -> Enum.at(alphabet, 11)
+    "M" -> Enum.at(alphabet, 12)
+    "N" -> Enum.at(alphabet, 13)
+    "O" -> Enum.at(alphabet, 14)
+    "P" -> Enum.at(alphabet, 15)
+    "Q" -> Enum.at(alphabet, 16)
+    "R" -> Enum.at(alphabet, 17)
+    "S" -> Enum.at(alphabet, 18)
+    "T" -> Enum.at(alphabet, 19)
+    "U" -> Enum.at(alphabet, 20)
+    "V" -> Enum.at(alphabet, 21)
+    "W" -> Enum.at(alphabet, 22)
+    "X" -> Enum.at(alphabet, 23)
+    "Y" -> Enum.at(alphabet, 24)
+    "Z" -> Enum.at(alphabet, 25)
+     _ -> letter
+  end
 end
 
-def letter_encryption(letter,alphabet)
-do
-
-  Enum.map(letter,fn x -> Cipher.change_letter(x, alphabet)end)
-end
-
-def letter_comparator(letter,alphabet)
-do
-  Enum.map(letter,fn x -> String.upcase(x,alphabet)end)
-end
-
-def store_txt(data) do
+def store_txt(data,outputf) do
   [data]
-  |> Enum.into(File.stream!("output.txt"))
+  |> Enum.into(File.stream!(outputf))
 end
 
-def change_letter(x, alphabet) do
-  case x do
-  "T"-> "Element Alphabet"
-  "t"-> "Element Alphabet 2"
-  _ -> "D"
-end
-
-end
 end
