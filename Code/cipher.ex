d5307ada1ce92624f40bf8c4423c31b94f31ce0d
A01700043 <33466790+A01700043@users.noreply.github.com>
2022-07-24 17:46:16 -0500

    Add files via upload

--- Code/cipher.ex
+++ Code/cipher.ex
@@ -1,126 +1,145 @@
-  # Final_Project_Keyword_Cipher
-  # Cristhian Estrada Quiroz  A01700043
-  # Jose Luis Mata A01377205
-  # 28/ 07 / 2022
+# Final Project: Keyword_Cipher
+# Cristhian Estrada A01700043
+# Jose Luis Mata A01377205
+# 28/07/2022
+
+# Cipher.main("input1.txt","output1.txt","dragon", :encrypt)
+# Cipher.main("output1.txt","output2.txt","dragon", :decrypt)
 
 defmodule  Cipher do
-  # Cipher.main("input1.txt","output1.txt","keyword", :encrypt)
-  # Cipher.main("output1.txt","output2.txt","keyword", :decrypt)
 
   @doc """
-     Recieves as arguments, input file path, output file path, keyword to cipher and operation atom.
-     This function call three functions that will allow us to read the file,
-     encrypt the content and write to the output file.
+    Recieves 4 arguments, inputf string to input-file path, outputf string to output-file path
+    keyword a string with key-word to encrypt or decrypt, operation an atom to :encrypt or :decrypt.
+
+    This function calls read_contents(), to stream input file content, calls cryptic_process() to encrypt
+    or decrypt input file data and store_txt to write the result in the output file.
   """
+
  def main(inputf,outputf,keyword,operation)
- do
-          inputf
-          |>Cipher.read_contents()
-          |>Cipher.cryptic_process(keyword,operation)
-          |>Cipher.store_txt(outputf)
- end
-  @doc """
-   Takes a string with the full path to a txt file.
-   It will open the file and return its contents in a list,
-   where each element is a row in the file.
-  """
-  def read_contents(inputf)
-    do
-     File.stream!(inputf)
-     |>Enum.map(fn x -> String.trim(x)end)
-     |>Enum.map(fn x -> String.split(x, ",")end)
+  do
+    inputf
+    |>Cipher.read_contents()
+    |>Cipher.cryptic_process(keyword,operation)
+    |>Cipher.store_txt(outputf)
   end
-    @doc """
-   Takes three arguments. A list of list with the content of input file,
-   a string with the keyword to use during encryption or decryption and
-   a atom that represents operation to do.
 
-   It will allow us to simplify data recovered from input file,
-   call needed functions to encrypt or decrypt,
-   and parse the information with symbols needed to represent rows in the txt file.
+  @doc """
+    Recieves 1 argument, inputf string to input-file path.
+    It will stream input file content row by rows and add them to a list of list.
   """
-  def cryptic_process(data,keyword,operation)
+
+ def read_contents(inputf)
+  do
+    File.stream!(inputf)
+    |>Enum.map(fn x -> String.trim(x)end)
+    |>Enum.map(fn x -> String.split(x, ",")end)
+  end
+
+  @doc """
+    Recieves 3 arguments. data a list of list with file content, keyword a string with
+    the key word to encrypt or decrypt, operation an atom with the operation to perform
+    :encrypt or :decrypt.
+
+    It will split every list of row content into words, call function letter parser() for
+    every word in order to encrypt or decrypt  and data cypher in order to prepare data for
+    write an output.
+  """
+
+ def cryptic_process(data,keyword,operation)
   do
     Enum.map(data,fn [x] -> String.split(x ,"", trim: true)end)
-     |>Enum.map(fn x -> Cipher.letter_parser(x,Cipher.cryptic_alphabet(keyword),operation)end)
-     |>Enum.map(fn x -> List.to_string(x)end)
-     |>Cipher.data_parser()
+    |>Enum.map(fn x -> Cipher.letter_parser(x,Cipher.cryptic_alphabet(keyword),operation)end)
+    |>Enum.map(fn x -> List.to_string(x)end)
+    |>Cipher.data_parser()
   end
-    @doc """
-   Recieves a list with the encrypted information,
-   it will allow us to add the symbols needed to represent rows in the txt file.
+
+  @doc """
+    Recieves 1 argument. data a list of list that represents encrypted or decrypted
+    rows from the original input file.
+
+    It joins every element in the list with the new line symbol.
   """
-  def data_parser(data)
+
+ def data_parser(data)
   do
     data
     |>Enum.map_join( "\n",&(&1))
   end
-    @doc """
-   Recieves as argument a keyword string,
-   it will create a 26 letters encrypted alphabet list,
-   that will allow us to represent indexes in encryption or decryption.
+
+  @doc """
+    Recieves 1 argument. keyword a string with key-word to encrypt or decrypt
+
+    It creates a list where every keyword letter is an element, then it is concatenated
+    with a list of 26 alphabet letters and repeated elements are deleted.
   """
-def cryptic_alphabet(keyword)
-   do
-   Enum.uniq(
-     (
-     keyword
-     |>String.upcase()
-     |>String.split("", trim: true))
-     ++ Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>
+
+ def cryptic_alphabet(keyword)
+  do
+    Enum.uniq((keyword
+      |>String.upcase()
+      |>String.split("", trim: true))
+      ++ Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>
     end))
-end
-     @doc """
+  end
 
-    Function that receives three arguments,
-   a list with letters of input file data content, an encrypted alphabet,
-   and an operation to realize.
+  @doc """
+    Recieves 3 arguments. letter a list of list with letters, alphabet the
+    result of the cryptic alphabet function, operation an atom to :encrypt or :decrypt.
 
-   It will simplify list of letters into items,
-   and call functions needed to encode or decode every letter.
+    It parses every letter in the list in order to prepare it for the encryption or decryption
+    process, then it calls letter_encryption () for every item.
   """
-def letter_parser(letter,alphabet,operation)
-do
-  Enum.map(letter,fn x ->
-    if String.match?(x, ~r/^\p{L}$/u)
-    do
-      if (x === String.upcase(x))
+
+ def letter_parser(letter,alphabet,operation)
+  do
+    Enum.map(letter,fn x -> if String.match?(x, ~r/^\p{L}$/u)
       do
-        Cipher.letter_encryption(x, alphabet, operation)
+        if (x === String.upcase(x))
+          do
+            Cipher.letter_encryption(x, alphabet, operation)
+          else
+            String.downcase(Cipher.letter_encryption(String.upcase(x), alphabet, operation))
+          end
       else
-        String.downcase(Cipher.letter_encryption(String.upcase(x), alphabet, operation))
-       end
-    else
-      x
-    end
-  end)
-end
-     @doc """
-   Function that recieves a letter, an encrypted alphabet,
-   and an operation to realize.
+        x
+      end
+    end)
+ end
 
-   It will match operation case and allow us to transform plain letters
-   into letters from the encrypted alphabet according to its matching index,
-   and other way around in case of decryption.
+  @doc """
+    Recieves 3 arguments, letter a string with a single letter, alphabet a list
+    with 26 letters, operation an atom to :encrypt or :decrypt.
+
+    It will match operation case and allow us to transform plain letters
+    into letters from the encrypted alphabet according to its matching index,
+    and other way around in case of decryption.
   """
-def letter_encryption(letter, alphabet,operation) do
-  case operation
-      do
-        :encrypt->
-           Enum.at(alphabet, Enum.find_index(Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>end), fn x -> x === letter end))
-       :decrypt->
-           Enum.at(Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>end), Enum.find_index(alphabet, fn x -> x === letter end))
-        _ -> "Unrecognized Operation"
+
+ def letter_encryption(letter, alphabet,operation)
+  do
+    case operation
+    do
+      :encrypt->
+        Enum.at(alphabet, Enum.find_index(Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>end), fn x -> x === letter end))
+
+      :decrypt->
+        Enum.at(Enum.map(Enum.to_list(?A..?Z), fn(n) -> <<n>>end), Enum.find_index(alphabet, fn x -> x === letter end))
+
+      _ -> "Unrecognized Operation"
     end
 
-end
-     @doc """
-   Takes a list of lists with the data, and a string with the full path to a file.
-   It will write the contents of the matrix into a file, in txt format.
+  end
+
+  @doc """
+    Recieves 2 arguments, data a list of strings, outputf string to output-file path.
+
+    It will write data stream into the specified output path.
   """
-def store_txt(data,outputf) do
-  [data]
-  |> Enum.into(File.stream!(outputf))
-end
+ def store_txt(data,outputf)
+  do
+    [data]
+    |> Enum.into(File.stream!(outputf))
+  end
 
 end
